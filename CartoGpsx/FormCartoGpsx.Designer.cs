402aa24979e65073a3af0d7d9bf2ba373f3814ea
jlemaire06 <57898082+jlemaire06@users.noreply.github.com>
2020-02-25 10:27:17 +0100

    Add files via upload

---
+++ CartoGpsx/FormCartoGpsx.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CartoGpsx
+{
+    partial class FormCartoGpsx
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnChangeMap = new System.Windows.Forms.Button();
+            this.sst = new System.Windows.Forms.StatusStrip();
+            this.tsslIco = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslGps = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslLon = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslLat = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslAlt = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslSat = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslHDOP = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslGpx = new System.Windows.Forms.ToolStripStatusLabel();
+            this.port = new System.IO.Ports.SerialPort(this.components);
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.sst.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChangeMap
+            // 
+            this.btnChangeMap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChangeMap.Location = new System.Drawing.Point(713, 389);
+            this.btnChangeMap.Name = "btnChangeMap";
+            this.btnChangeMap.Size = new System.Drawing.Size(75, 23);
+            this.btnChangeMap.TabIndex = 1;
+            this.btnChangeMap.Text = "Change";
+            this.btnChangeMap.UseVisualStyleBackColor = true;
+            this.btnChangeMap.Click += new System.EventHandler(this.BtnChangeMap_Click);
+            // 
+            // sst
+            // 
+            this.sst.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslIco,
+            this.tsslGps,
+            this.tsslLon,
+            this.tsslLat,
+            this.tsslAlt,
+            this.tsslSat,
+            this.tsslHDOP,
+            this.tsslGpx});
+            this.sst.Location = new System.Drawing.Point(0, 428);
+            this.sst.Name = "sst";
+            this.sst.Size = new System.Drawing.Size(800, 22);
+            this.sst.TabIndex = 2;
+            // 
+            // tsslIco
+            // 
+            this.tsslIco.Image = global::CartoGpsx.Resource.ko;
+            this.tsslIco.Name = "tsslIco";
+            this.tsslIco.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.tsslIco.Size = new System.Drawing.Size(26, 17);
+            // 
+            // tsslGps
+            // 
+            this.tsslGps.Image = global::CartoGpsx.Resource.GPS;
+            this.tsslGps.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsslGps.Name = "tsslGps";
+            this.tsslGps.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.tsslGps.Size = new System.Drawing.Size(60, 17);
+            this.tsslGps.Click += new System.EventHandler(this.TsslGps_Click);
+            // 
+            // tsslLon
+            // 
+            this.tsslLon.Name = "tsslLon";
+            this.tsslLon.Size = new System.Drawing.Size(27, 17);
+            this.tsslLon.Text = "Lon";
+            // 
+            // tsslLat
+            // 
+            this.tsslLat.Name = "tsslLat";
+            this.tsslLat.Size = new System.Drawing.Size(23, 17);
+            this.tsslLat.Text = "Lat";
+            // 
+            // tsslAlt
+            // 
+            this.tsslAlt.Name = "tsslAlt";
+            this.tsslAlt.Size = new System.Drawing.Size(22, 17);
+            this.tsslAlt.Text = "Alt";
+            // 
+            // tsslSat
+            // 
+            this.tsslSat.Name = "tsslSat";
+            this.tsslSat.Size = new System.Drawing.Size(23, 17);
+            this.tsslSat.Text = "Sat";
+            // 
+            // tsslHDOP
+            // 
+            this.tsslHDOP.Name = "tsslHDOP";
+            this.tsslHDOP.Size = new System.Drawing.Size(40, 17);
+            this.tsslHDOP.Text = "HDOP";
+            // 
+            // tsslGpx
+            // 
+            this.tsslGpx.Image = global::CartoGpsx.Resource.GPX;
+            this.tsslGpx.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsslGpx.Name = "tsslGpx";
+            this.tsslGpx.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.tsslGpx.Size = new System.Drawing.Size(60, 17);
+            this.tsslGpx.Click += new System.EventHandler(this.TsslGpx_Click);
+            // 
+            // port
+            // 
+            this.port.PortName = "COM27";
+            this.port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.Port_DataReceived);
+            // 
+            // gmap
+            // 
+            this.gmap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(0, 1);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 20;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(800, 424);
+            this.gmap.TabIndex = 3;
+            this.gmap.Zoom = 0D;
+            // 
+            // FormCartoGpsx
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sst);
+            this.Controls.Add(this.btnChangeMap);
+            this.Controls.Add(this.gmap);
+            this.Name = "FormCartoGpsx";
+            this.Text = "CartoGPSX";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormCartoGpsx_FormClosing);
+            this.Load += new System.EventHandler(this.FormCartoGpsx_Load);
+            this.sst.ResumeLayout(false);
+            this.sst.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeMap;
+        private System.Windows.Forms.StatusStrip sst;
+        private System.Windows.Forms.ToolStripStatusLabel tsslIco;
+        private System.IO.Ports.SerialPort port;
+        private System.Windows.Forms.ToolStripStatusLabel tsslGpx;
+        private System.Windows.Forms.ToolStripStatusLabel tsslGps;
+        private System.Windows.Forms.ToolStripStatusLabel tsslLon;
+        private System.Windows.Forms.ToolStripStatusLabel tsslLat;
+        private System.Windows.Forms.ToolStripStatusLabel tsslAlt;
+        private System.Windows.Forms.ToolStripStatusLabel tsslSat;
+        private System.Windows.Forms.ToolStripStatusLabel tsslHDOP;
+        private GMap.NET.WindowsForms.GMapControl gmap;
+    }
+}
---
+++ CartoGpsx/FormCartoGpsx.cs
@@ -0,0 +1,329 @@
+﻿// FormCartoGpsx.cs
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System.Data;
+using System.Linq;
+using ORMi;
+
+namespace CartoGpsx
+{
+    [WMIClass("Win32_PnPEntity")]
+    public class Device
+    {
+        // Pour GetPortName()
+        public string Name { get; set; }
+    }
+
+    public partial class FormCartoGpsx : Form
+    {
+        //***************************************************************************************************
+        // Variables globales
+        //***************************************************************************************************
+
+        private int numMap;                // Numéro de carte affichée : 0 => Google sat, 1 => OpenTopoMap
+        private bool okGps;                // Vrai si position GPS à afficher
+        private bool okGpx;                // Vrai si route GPS à afficher
+        private String gpxFileName;        // Nom du fichier Gpx à afficher
+
+        //***************************************************************************************************
+        // Init et End
+        //***************************************************************************************************
+
+        public FormCartoGpsx()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void FormCartoGpsx_Load(object sender, EventArgs e)
+        {
+            // Port série
+            try
+            {
+                port.PortName = GetPortName();
+            }
+            catch { }
+            tsslIco.Image = Resource.ko;
+            PortOpen();
+
+            // Carte
+            numMap = Set.Default.numMap;
+            SetMap(numMap);
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            gmap.CacheLocation = @".\cache\";
+            gmap.MapScaleInfoEnabled = true;
+            gmap.Zoom = Set.Default.zoomMap;
+            gmap.Size = new Size(this.Width, this.Height);
+            gmap.Position = new PointLatLng(Set.Default.latCenterMap, Set.Default.lngCenterMap);
+            gmap.ShowCenter = false;
+
+            // Overlay avec marqueur et route
+            GMapOverlay ovl = new GMapOverlay("ovl");
+            gmap.Overlays.Add(ovl);
+            GMapMarker marker = new GMarkerGoogle(new PointLatLng(), GMarkerGoogleType.red_dot);
+            marker.IsVisible = false;
+            ovl.Markers.Add(marker);
+            GMapRoute route = new GMapRoute(new List<PointLatLng>(), "");
+            route.Stroke = new Pen(Color.Red, 4);
+            route.IsVisible = false;
+            ovl.Routes.Add(route);
+            okGps = Set.Default.okGps;
+            okGpx = Set.Default.okGpx;
+            if (!okGps) EraseGps();
+            if (okGpx)
+            {
+                gpxFileName = Set.Default.gpxFileName;
+                SetGpx(gpxFileName);
+            }
+        }
+
+        private void FormCartoGpsx_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Set.Default.numMap = numMap;
+            Set.Default.zoomMap = gmap.Zoom;
+            Set.Default.latCenterMap = gmap.Position.Lat;
+            Set.Default.lngCenterMap = gmap.Position.Lng;
+            Set.Default.okGps = okGps;
+            Set.Default.okGpx = okGpx;
+            Set.Default.gpxFileName = gpxFileName;
+            Set.Default.Save();
+        }
+
+
+        //***************************************************************************************************
+        // Map
+        //***************************************************************************************************  
+
+        void SetMap(int numMap)
+        {
+            switch (numMap)
+            {
+                case 0:
+                    gmap.MapProvider = GMap.NET.MapProviders.GoogleSatelliteMapProvider.Instance;
+                    Text = "CartoGPSX : Google Satellite";
+                    break;
+                case 1:
+                    gmap.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+                    Text = "CartoGPSX : Open Topo Map";
+                    break;
+            }
+        }
+
+        private void BtnChangeMap_Click(object sender, EventArgs e)
+        {
+            numMap = 1 - numMap;
+            SetMap(numMap);
+        }
+
+        //***************************************************************************************************
+        // Gps
+        //***************************************************************************************************  
+
+        double Degree(string str)
+        {
+            double a = double.Parse(str.Replace('.', ',')) / 100;
+            double e = Math.Truncate(a);
+            return e + (a - e) / 0.6;
+        }
+
+        void SetGps(double lon, double lat, float alt, int sat, float hdop)
+        {
+            tsslLon.Text = "Lon : " + lon.ToString("#.####0°");
+            tsslLat.Text = "Lat : " + lat.ToString("#.####0°");
+            tsslAlt.Text = "Alt : " + alt.ToString("###m");
+            tsslSat.Text = "Sat : " + sat.ToString("#");
+            tsslHDOP.Text = "HDOP : " + hdop.ToString("0.#0");
+            gmap.Overlays[0].Markers[0].Position = new PointLatLng(lat, lon);
+            gmap.Overlays[0].Markers[0].IsVisible = true;
+        }
+
+        void EraseGps()
+        {
+            try { gmap.Overlays[0].Markers[0].IsVisible = false; }
+            catch { }
+            tsslLon.Text = "";
+            tsslLat.Text = "";
+            tsslAlt.Text = "";
+            tsslSat.Text = "";
+            tsslHDOP.Text = "";
+        }
+
+        private void TsslGps_Click(object sender, EventArgs e)
+        {
+            if (!port.IsOpen) return;
+            okGps = !okGps;
+            if (!okGps) EraseGps();
+        }
+
+        
+        //***************************************************************************************************
+        // Gpx
+        //***************************************************************************************************  
+
+        private void SetGpx(String strFileName)
+        {
+            try
+            {
+                gpxType gpx = gmap.Manager.DeserializeGPX(File.ReadAllText(strFileName));
+                if (gpx != null)
+                {
+                    if (gpx.trk.Length > 0)
+                    {
+                        gmap.Overlays[0].Routes[0].Points.Clear();
+                        foreach (var trk in gpx.trk)
+                        {
+                            List<PointLatLng> points = new List<PointLatLng>();
+                            foreach (var seg in trk.trkseg)
+                            {
+                                foreach (var p in seg.trkpt)
+                                {
+                                    points.Add(new PointLatLng((double)p.lat, (double)p.lon));
+                                }
+                            }
+                            gmap.Overlays[0].Routes[0].Points.AddRange(points);
+                        }
+                        tsslGpx.Text = Path.GetFileName(strFileName);
+                        gmap.Overlays[0].Routes[0].IsVisible = true;
+                        // gmap.ZoomAndCenterRoutes(null);
+                        okGpx = true;
+                        gpxFileName = strFileName;
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void TsslGpx_Click(object sender, EventArgs e)
+        {
+            okGpx = !okGpx;
+            switch (okGpx)
+            {
+                case true:
+                    using (FileDialog dlg = new OpenFileDialog())
+                    {
+                        dlg.CheckPathExists = true;
+                        dlg.CheckFileExists = false;
+                        dlg.AddExtension = true;
+                        dlg.DefaultExt = "gpx";
+                        dlg.ValidateNames = true;
+                        dlg.Title = "Afficher une route .Gpx";
+                        dlg.Filter = "gpx files (*.gpx)|*.gpx";
+                        dlg.FilterIndex = 1;
+                        dlg.RestoreDirectory = true;
+                        if (dlg.ShowDialog() == DialogResult.OK) SetGpx(dlg.FileName);
+                    }
+                    break;
+                case false:
+                    gmap.Overlays[0].Routes[0].IsVisible = false;
+                    tsslGpx.Text = "";
+                    break;
+            }
+        }
+
+        //***************************************************************************************************
+        // Port série
+        //***************************************************************************************************
+        private String GetPortName()
+        {
+            String str = "";
+            WMIHelper helper = new WMIHelper("root\\CimV2");
+            Device device = helper.Query<Device>().ToList()
+                .Where(p => (p.Name ?? "")
+                .Contains("u-blox GNSS Receiver")).SingleOrDefault();
+            if (device != null)
+            {
+                str = device.Name;
+                int i = str.IndexOf("(");
+                int j = str.LastIndexOf(")");
+                str = str.Substring(i + 1, j - i - 1);
+            }
+            return str;
+        }
+
+        private void PortOpen()
+        {
+            try
+            {
+                port.Open();
+                tsslIco.Image = Resource.ok;
+                okGps = true;
+            }
+            catch
+            {
+                EraseGps();
+            }
+        }
+
+        private void PortClose()
+        {
+            try
+            {
+                if (port.IsOpen) port.Close();
+                tsslIco.Image = Resource.ko;
+                EraseGps();
+            }
+            catch
+            {
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_DEVICECHANGE = 0x0219;
+            const int DBT_DEVICEARRIVAL = 0x8000;
+            const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+
+            if (m.Msg == WM_DEVICECHANGE)
+            {
+                switch (m.WParam.ToInt32())
+                {
+                    case DBT_DEVICEARRIVAL:
+                        PortOpen();
+                        break;
+                    case DBT_DEVICEREMOVECOMPLETE:
+                        PortClose();
+                        break;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if (port.BytesToRead < 90) return;
+                string str = port.ReadExisting();
+                if (!okGps) return;
+                int deb = str.IndexOf("$GNGGA");
+                if (deb >= 0)
+                {
+                    int fin = str.IndexOf('\n', deb);
+                    if (fin > deb)
+                    {
+                        str = str.Substring(deb, fin - deb + 1);
+                        string[] tabStr = str.Split(',');
+                        double lon = Degree(tabStr[4]);
+                        double lat = Degree(tabStr[2]);
+                        float alt = float.Parse(tabStr[9].Replace('.', ','));
+                        int sat = int.Parse(tabStr[7]);
+                        float hdop = float.Parse(tabStr[8].Replace('.', ','));
+                        if (okGps) SetGps(lon, lat, alt, sat, hdop);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
